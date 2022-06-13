a1703894b765523707a4e9e031c4446c282efd68
Manabu Niseki <manabu.niseki@gmail.com>
2022-06-14 08:48:32 +0900

    refactor: change header for API key

--- misp_gateway/api/v1/dependencies.py
+++ misp_gateway/api/v1/dependencies.py
@@ -6,9 +6,9 @@ from misp_gateway.core import settings
 from misp_gateway.forwarder import Forwarder
 
 
-def is_truthy(v: Optional[Any]) -> bool:
+def is_truthy(v: Optional[Any], *, default: bool = True) -> bool:
     if v is None:
-        return True
+        return default
 
     if isinstance(v, bool):
         return v
@@ -18,6 +18,6 @@ def is_truthy(v: Optional[Any]) -> bool:
 
 def get_forwarder(request: Request) -> Forwarder:
     misp_url = str(request.headers.get("x-misp-url", settings.MISP_URL))
-    misp_api_key = str(request.headers.get("x-misp-api-key", settings.MISP_API_KEY))
+    misp_api_key = str(request.headers.get("authorization", settings.MISP_API_KEY))
     verify_ssl = request.headers.get("x-misp-verify-ssl", settings.MISP_VERIFY_SSL)
     return Forwarder(misp_url, misp_api_key, verify_ssl=is_truthy(verify_ssl))
